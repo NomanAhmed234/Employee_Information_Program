fa841f6a13d367980f901a0d47f77ad987374227
Noman Ahmed <nomanpb234@gmail.com>
2023-07-23 23:29:33 +0500

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6q4
+{
+    internal class Employee
+    {
+        public string name, dept, design;
+        public void EmpName()
+        {
+            Console.WriteLine("Give your Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Register Employee's Name: {0}\n", name);
+        }
+        public void DeptList()
+        {
+            Console.WriteLine("Give your Department: ");
+            dept = Console.ReadLine();
+            Console.WriteLine("Employee's Department: {0}\n",dept);
+        }
+        
+        public void EmpDesign()
+        {
+            Console.WriteLine("Give your Designation: ");
+            design = Console.ReadLine();
+            Console.WriteLine("Employee's current Designation: {0}\n", design);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*Write a C# code which created a public class Employee with its public member variables name ,
+dept and design. Now create method EmpName( ) which takes user input for employee name
+and then pint the name also. Create another method DeptList( ) that takes input the name of
+department of employee and third method as EmpDesign( ) which just takes input about
+employee’s designation. In main call these methods and print appropriate results.*/
+
+namespace Lab_6q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.EmpName();
+            emp1.DeptList();
+            emp1.EmpDesign();
+        }
+    }
+}
